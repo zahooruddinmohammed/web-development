c697b071495f2bbbdbaac8549e97c0d9d3b9e9d4
zahooruddinmohammed <zm254@njit.edu>
2023-09-23 03:29:42 -0400

    adding name

--- M2/Probelm3.py
+++ M2/Probelm3.py
@@ -16,7 +16,7 @@ def process_array(num, arr):
             pos_arr.append(pos_x)
             #print(f"{abs(x)} ({type(x)})")
             print(f"({type(pos_x)}, {pos_x})", end=' ')
-            #type_and_value.append(f"({pos_x})")
+            #type_and_value.append(f"({pos_x})") 
             
         elif isinstance(x, str): 
             if x.startswith('-'):
@@ -35,7 +35,7 @@ def process_array(num, arr):
     #print( pos_arr)    
     #print(" ".join(type_and_value))
 
-
+    #zm254-09/23/23
     # Note: use the arr variable; don't directly refer to a1-a4 variables
     # TODO add new code here to print the desired result
     # TODO include the type() of the output data to ensure the result is positive AND the same datatype as the input value
