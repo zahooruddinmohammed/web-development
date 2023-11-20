fd145ff2d943c5a1016aec7f35a4cfac611a7c87
zahooruddinmohammed <zm254@njit.edu>
2023-11-20 03:23:47 -0500

    completing donations.py todo edit

--- MP3/views/donations.py
+++ MP3/views/donations.py
@@ -170,9 +170,11 @@ def edit():
     row = {}
     
     # TODO edit-1 request args id is required (flash proper error message)
-    id = False
+    input = request.form
+    id = request.args.get("id")
     if not id: # TODO update this for TODO edit-1
-        pass
+       flash("Company ID is not available","Danger")
+       redirect('/employee/search')
     else:
         if request.method == "POST":
             
@@ -187,37 +189,97 @@ def edit():
             # TODO add-9 item_quantity is required and must be more than 0 (flash proper error message)
             # TODO add-10 donation_date is required and must be within the past 30 days
             # TODO add-11 comments are optional
+            form_value={}
             has_error = False # use this to control whether or not an insert occurs
-                
+            form_value["donor_firstname"] = input.getlist("donor_firstname")
+            form_value["donor_lastname"] = input.getlist("donor_lastname")
+            form_value["donor_email"] = input.getlist("donor_email")
+            form_value["organization_id"]= input.getlist("organization_id")
+            form_value["item_name"]= input.getlist("item_name")
+            form_value["item_description"]= input.getlist("item_description")
+            form_value["item_quantity"]=input.getlist("item_quantity")
+            form_value["donation_date"]=input.getlist("donation_date")
+            form_value["comments"]=input.getlist("comments")
+
+            for field,values in form_value.items():
+                for  value in values:
+                    #todo edit-2
+                    if not value and field == "donor_firstname":
+                        flash("Donor First Name is required.", "danger")
+                        has_error =True
+                    #todo edit-3
+                    elif not value and field == "donor_lastname":
+                        flash("Donor Last Name is required.","danger")
+                        has_error =True
+                    #todo edit-4
+                    elif not value and field == "donor_email" and not re.match(r"[^@]+@[^@]+\.[^@]+", value):
+                        flash("Invalid email format. Please enter a valid email address", "danger")
+                        has_error = True
+                    #todo edit-5
+                    elif not value and field == "organization_id":
+                        flash("Organization ID is required.","danger")
+                        has_error =True
+                    #todo edit-6
+                    elif not value and field == "item_name":
+                        flash("Item name is required.","danger")
+                        has_error =True
+                    
+                    #todo edit-8
+                    elif not value and field =="item_quantity":
+                        flash("Item quantity is required.","danger")
+                        has_error =True
+                    
+                    elif not value.isnumeric() or int(value) <=0:
+                        flash("Item quantity must be a positive number.","danger")
+                        has_error =True
+                    #todo edit-9
+                    elif not value and field == "donation_date":
+                        try:
+                            donation_date = datetime.strptime(form_value["donation_date"], "%Y-%m-%d")
+                            thirty_days_ago = datetime.now() - timedelta(days=30)
+                            if donation_date < thirty_days_ago:
+                                flash("Donation date must be within the past 30 days.", "danger")
+                                has_error = True
+                        except ValueError:
+                            flash("Invalid date format. Please use YYYY-MM-DD.", "danger")
+                            has_error = True
+
             if not has_error:
                 try:
                     # TODO edit-12 fill in proper update query
                     result = DB.update("""
-                    UPDATE ... SET
-                    ...
-                    """, ...)
+                    UPDATE IS601_MP3_Donations SET
+                    donor_firstname = %(donor_firstname)s,
+                    donor_lastname = %(donor_lastname)s,
+                    donor_email = %(donor_email)s,
+                    organization_id = %(organization_id)s,
+                    item_name = %(item_name)s,
+                    item_description = %(item_description)s,
+                    donation_date = %(donation_date)s
+                    WHERE id = %(donation_id)s
+                    """, form_value)
                     
                     if result.status:
                         flash("Updated record", "success")
                 except Exception as e:
                     # TODO edit-13 make this user-friendly
                     print(f"update error {e}")
-                    flash(e, "danger")
+                    flash("Failed to update record. Please try again.", "danger")
         
         try:
             # TODO edit-14 fetch the updated data 
-            result = DB.selectOne("""SELECT 
-            ...
-            FROM ... LEFT JOIN ... 
-              
-             WHERE ..."""
-            , id)
+            result = DB.selectOne("""SELECT d.id, d.donor_firstname, d.donor_lastname, d.donor_email, d.organization_id, 
+                       d.item_name, d.item_description, d.item_quantity, d.donation_date, d.comments,o.organization_name
+                        FROM IS601_MP3_Donations d
+                        LEFT JOIN IS601_MP3_Organizations o ON d.organization_id = o.id
+                        WHERE d.id = %(id)s""", {"id": id})
             
             if result.status:
                 row = result.row
         except Exception as e:
             # TODO edit-15 make this user-friendly
-            flash(str(e), "danger")
+            print(e)
+            flash(f"Unexpected error while trying to fetch updated data: {e}", "danger")
     
     return render_template("manage_donation.html", donation=row)
 
